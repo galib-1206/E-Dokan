443ad9b15b2a407fd986d93c083358d753ead9a9
galib-1206 <bsse1206@iit.du.ac.bd>
2024-05-09 22:19:46 +0600

    item_list view implemented on browser

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+def item_list (req):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(req, "item_list.html", context)
