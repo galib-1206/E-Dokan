0362d3ebb83bdb5e082bf6593e44e4834c082ca1
galib-1206 <bsse1206@iit.du.ac.bd>
2024-05-23 20:31:37 +0600

    ProductList component  created

---
+++ src/containers/ProductList.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import axios from "axios";
+import {
+    ItemMeta,
+    ItemImage,
+    ItemHeader,
+    ItemGroup,
+    ItemExtra,
+    ItemDescription,
+    ItemContent,
+    Button,
+    Icon,
+    Image,
+    Item,
+    Label,
+    Container,
+    Dimmer,
+    Loader,
+    Message,
+    Segment
+} from 'semantic-ui-react'
+
+const paragraph = <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+
+class ProductList extends React.Component {
+    state = {
+        loading: false,
+        error: null,
+        data: []
+    };
+
+    componentDidMount() {
+        this.setState({ loading: true });
+        axios
+            .get("/some_url")
+            .then(res => {
+                this.setState({ data: res.data, loading: false });
+            })
+            .catch(err => {
+                this.setState({ error: err, loading: false });
+            });
+    }
+    render() {
+        const { data, error, loading } = this.state;
+
+        return (
+
+            <Container>
+                {error && (
+                    <Message
+                        error
+                        header="There was some errors with your submission"
+                        content={JSON.stringify(error)}
+                    />
+                )}
+                {loading && (
+                    <Segment>
+                        <Dimmer active inverted>
+                            <Loader inverted>Loading</Loader>
+                        </Dimmer>
+
+                        <Image src="/images/wireframe/short-paragraph.png" />
+                    </Segment>
+                )}
+                <ItemGroup divided>
+                    <Item>
+                        <ItemImage src='https://react.semantic-ui.com/images/wireframe/image.png' />
+
+                        <ItemContent>
+                            <ItemHeader as='a'>My Neighbor Totoro</ItemHeader>
+                            <ItemMeta>
+                                <span className='cinema'>IFC Cinema</span>
+                            </ItemMeta>
+                            <ItemDescription>{paragraph}</ItemDescription>
+                            <ItemExtra>
+                                <Button primary floated='right' icon labelPosition='right'>
+                                    Add cart
+                                    <Icon name='cart plus' />
+                                </Button>
+                                <Label>Limited</Label>
+                            </ItemExtra>
+                        </ItemContent>
+                    </Item>
+                </ItemGroup>
+            </Container>
+
+        )
+    }
+}
+
+export default ProductList;
